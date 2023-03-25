1250138a18dbf817ed4ae33ae46ec1ead15cc586
prince475 <danielmwai475@gmail.com>
2023-03-25 22:35:32 +0300

    Updating our JSX to allow you to write a joke with just the punchline

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ function App() {
   return (
     <div className="App">
       <Joke
+        punchline="It's hard to explain puns to kleptomaniacs because they always take things literally"
+      />
+      <Joke
         setup=" I got my daughter a fridge for her birthday?"
         punchline="I can't wait to see her face light up when she opens it."
        />
--- src/components/Joke.js
+++ src/components/Joke.js
@@ -5,8 +5,9 @@ function Joke(props) {
 
     return (
         <div className="cards">
-            <h3>{props.setup}</h3>
-            <h2>{props.punchline}</h2>
+        {/* {props.setup && <h3>Setup: {props.setup}</h3>} */}
+            {<h3 style={{display: props.setup ? "block" : "none"}}>Setup: {props.setup}</h3>}
+            {<p>Punchline: {props.punchline}</p>}
         </div>
     );
 
